87141f9d1c06204be7350cce897fea5de46060b0
Junho Choi <chlwnsgh311@gmail.com>
2023-12-30 18:19:15 +0900

    완료 (#6)
    * 완료
    
    * 테스트 코드 추가 익셉션 테스트

--- src/main/java/com/example/be_study/choi/utils/IdentityValidateUtils.java
+++ src/main/java/com/example/be_study/choi/utils/IdentityValidateUtils.java
@@ -1,15 +1,48 @@
 package com.example.be_study.choi.utils;
 
+import org.springframework.util.ObjectUtils;
+
 public class IdentityValidateUtils {
 
-    public static boolean isIdentityNationalUser(String identity){
+    public static int END_IDENTITY_FIRST_WORD_INDEX = 6;
+    public static int IDENTITY_SIZE = 13;
+    //REGEX
+    public static String REGEX_ALL_NUMBER = "[0-9]+"; //전부 숫자인지 검증합니다.
 
-        return true;
+    //Error Message
+    public static String ERROR_NULL_OR_EMPTY_IDENTITY = "주민등록번호가 Null이거나 비어있습니다.";
+    public static String ERROR_LENGTH_IDENTITY = "주민등록번호 자릿수가 올바르지 않습니다.";
+    public static String ERROR_CONTAIN_CHARACTER = "주민등록번호에 숫자가 아닌 값이 섞여있습니다.";
+    public static String ERROR_BAD_END_FIRST_WORD = "주민등록번호의 정보가 잘못됬습니다 - 내국인 외국인인지의 범위에 하나도 포함되지 않음";
+
+    public static void validationIdentity(String identity) {
+        if (ObjectUtils.isEmpty(identity)) {
+            throw new IllegalArgumentException(ERROR_NULL_OR_EMPTY_IDENTITY);
+        }
+        if (IDENTITY_SIZE != identity.length()) {
+            throw new IllegalArgumentException(ERROR_LENGTH_IDENTITY);
+        }
+        if (!identity.matches(REGEX_ALL_NUMBER)) {
+            throw new IllegalArgumentException(ERROR_CONTAIN_CHARACTER);
+        }
+
+        int firstWord = (int) identity.charAt(END_IDENTITY_FIRST_WORD_INDEX) - '0';
+
+        if (firstWord < 0 || firstWord > 8) {
+            throw new IllegalArgumentException(ERROR_BAD_END_FIRST_WORD);
+        }
     }
 
-    public static boolean isIdentityForeignerUser(String identity){
+    public static boolean isIdentityNationalUser(String identity) {
+        validationIdentity(identity);
+        int firstWord = (int) identity.charAt(END_IDENTITY_FIRST_WORD_INDEX) - '0';
+        return (firstWord > 0 && firstWord < 5);
+    }
 
-        return true;
+    public static boolean isIdentityForeignerUser(String identity) {
+        validationIdentity(identity);
+        int firstWord = (int) identity.charAt(END_IDENTITY_FIRST_WORD_INDEX) - '0';
+        return (firstWord > 4 && firstWord < 9);
     }
 
 }
--- src/test/java/com/example/be_study/choi/utils/IdentityValidateUtilsTests.groovy
+++ src/test/java/com/example/be_study/choi/utils/IdentityValidateUtilsTests.groovy
@@ -4,14 +4,57 @@ import spock.lang.Specification
 
 class IdentityValidateUtilsTests extends Specification {
 
-    def "주민등록번호가 내국인이여합니다"() {
+    def "주민등록번호가 내국인이여합니다_단건"() {
         given:
-        String identity = "9302251111111";
+        String identity = "9302251111111"
 
         when:
         def result =  IdentityValidateUtils.isIdentityNationalUser(identity);
 
         then:
-        !result
+        result
+    }
+
+    def "주민등록번호가 내국인이여합니다_다건"() {
+        expect:
+        IdentityValidateUtils.isIdentityNationalUser(identity)
+
+        where:
+        identity << [
+                '9302251111111',
+                '9302252111111',
+                '9302253111111',
+                '9302254111111'
+        ]
+    }
+
+    def "주민등록번호가 외국인이여합니다_다건"() {
+        expect:
+        IdentityValidateUtils.isIdentityForeignerUser(identity)
+
+        where:
+        identity << [
+                '9302255111111',
+                '9302256111111',
+                '9302257111111',
+                '9302258111111'
+        ]
+    }
+
+    def "주민등록번호가 자리수가 안맞으면 에러를 표시합니다."() {
+        when:
+        IdentityValidateUtils.isIdentityForeignerUser(identity)
+
+        then:
+        def e = thrown(IllegalArgumentException.class)
+        IdentityValidateUtils.ERROR_LENGTH_IDENTITY == e.message
+
+        where:
+        identity << [
+                '930225511111',
+                '93022561111',
+                '930225711',
+                '93022581'
+        ]
     }
 }
