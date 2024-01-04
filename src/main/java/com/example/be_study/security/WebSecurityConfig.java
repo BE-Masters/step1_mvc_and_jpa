8ddb188820c3204eecc38d7ce88d52943be70674
KEJdev <kej12516@gmail.com>
2024-01-04 20:22:38 +0900

    - Local, Prod 프로필 환경 세팅함

--- src/main/java/com/example/be_study/security/WebSecurityConfig.java
+++ src/main/java/com/example/be_study/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.be_study.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,10 +11,20 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    @Profile("local")
+    public SecurityFilterChain localSecurityFilterChain(HttpSecurity http) throws Exception {
 
         http.httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable); // Spring Security 기본 인증 비활성화
+                .csrf(AbstractHttpConfigurer::disable); // 로컬 환경에서는 기본 인증과 CSRF 보호 비활성화
         return http.build();
     }
+
+    @Bean
+    @Profile("prod")
+    public SecurityFilterChain prodSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable); // TODO 프로덕션 환경에 맞는 보안 구성 추가해야댐
+        return http.build();
+    }
+
 }
