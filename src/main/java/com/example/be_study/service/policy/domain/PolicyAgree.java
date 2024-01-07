ebee209880e3ae96c7103752e688911eda32054e
OreoChoi <chlwnsgh311@gmail.com>
2024-01-07 19:57:34 +0900

    엔티티 예제 추가

--- src/main/java/com/example/be_study/service/policy/domain/PolicyAgree.java
+++ src/main/java/com/example/be_study/service/policy/domain/PolicyAgree.java
@@ -1,4 +1,21 @@
 package com.example.be_study.service.policy.domain;
 
-public class PolicyAgree {
+import com.example.be_study.service.base.AbstractBaseEntity;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(indexes = {})
+@AttributeOverride(name = "id", column = @Column(name = "policy_agree_id"))
+@Entity(name = "policy_agree")
+public class PolicyAgree extends AbstractBaseEntity {
+
+
+
+
 }
