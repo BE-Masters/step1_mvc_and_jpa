ff57428f5d6506b61c4e9d8470a81eff126085f5
OreoChoi <chlwnsgh311@gmail.com>
2024-01-08 11:41:43 +0900

    상속 클래스명 변경

--- src/main/java/com/example/be_study/service/base/AbstractBaseUserByEntity.java
+++ src/main/java/com/example/be_study/service/base/AbstractBaseUserByEntity.java
@@ -4,18 +4,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 @Setter
 @EntityListeners(AuditingEntityListener.class)
-public abstract class AbstractBaseTimeEntity extends AbstractBaseEntity{
+public abstract class AbstractBaseUserByEntity extends AbstractBaseEntity{
 
     @CreatedBy
     @Column(name = "created_by", nullable = false)
