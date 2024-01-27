cfd5e8a24e534b680006176dfd42f83aa312c21e
yugyeong71 <youkyoung71@naver.com>
2024-01-28 00:50:02 +0900

    이메일 전송 API 테스트 코드 작성

--- src/test/java/com/example/be_study/user/login/EmailServiceTest.groovy
+++ src/test/java/com/example/be_study/user/login/EmailServiceTest.groovy
@@ -2,6 +2,7 @@ package com.example.be_study.user.login
 
 import com.example.be_study.service.user.service.EmailService
 import com.example.be_study.service.user.service.RedisService
+import jakarta.mail.internet.MimeMessage
 import org.springframework.mail.javamail.JavaMailSender
 import spock.lang.Specification
 import spock.lang.Subject
@@ -14,6 +15,8 @@ class EmailServiceTest extends Specification {
     JavaMailSender javaMailSender = Mock()
 
     RedisService redisService = Mock()
+
+    MimeMessage mimeMessage = Mock(MimeMessage)
 
     def setup() {
         emailService = new EmailService(
--- src/test/java/com/example/be_study/user/login/EmailServiceVerifyTest.groovy
+++ src/test/java/com/example/be_study/user/login/EmailServiceVerifyTest.groovy
@@ -1,6 +1,7 @@
 package com.example.be_study.user.login
 
 import com.example.be_study.service.user.enums.UserSignUpResponseCode
+import jakarta.mail.internet.MimeMessage
 
 class EmailServiceVerifyTest extends EmailServiceTest {
 
@@ -50,4 +51,34 @@ class EmailServiceVerifyTest extends EmailServiceTest {
         result.message == UserSignUpResponseCode.EXPIRED_AUTH_MAIL_CODE.getResponseMessage()
     }
 
+    def "메일 전송 성공"() {
+        given:
+        String email = "test@example.com"
+
+        1 * javaMailSender.createMimeMessage() >> { Mock(MimeMessage) }
+
+        and:
+        javaMailSender.send(mimeMessage)
+
+        when:
+        def result = emailService.sendMail(email)
+
+        then:
+        result.message == UserSignUpResponseCode.SUCCESS.getResponseMessage()
+    }
+
+    def "메일 전송 실패"() {
+        given:
+        String email = "test@example.com"
+
+        1 * redisService.existData(email) >> true
+        1 * javaMailSender.createMimeMessage() >> { Mock(MimeMessage) }
+        1 * redisService.deleteData(email) >> null
+
+        when:
+        def result = emailService.sendMail(email)
+
+        then:
+        result.status == UserSignUpResponseCode.MAIL_SEND_FAILED.getResponseStatus()
+    }
 }
