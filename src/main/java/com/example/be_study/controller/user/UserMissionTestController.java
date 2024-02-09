7320d42919c31c3a2c4bda8442fd3d3c31c7da8c
KEJdev <kej12516@gmail.com>
2024-02-10 00:35:04 +0900

    [미션3] 페이징 50개 단위로 페이징하는 API 만들었습니다.
    테스트는 아래의 API로 하실 수 있습니다.
    * http://localhost:8080/api/v1/user/paging

--- src/main/java/com/example/be_study/controller/user/UserMissionTestController.java
+++ src/main/java/com/example/be_study/controller/user/UserMissionTestController.java
@@ -1,20 +1,30 @@
 package com.example.be_study.controller.user;
 
 import com.example.be_study.common.response.DataResponse;
-import com.example.be_study.common.response.DataResponseCode;
 import com.example.be_study.security.CurrentUser;
+import com.example.be_study.service.user.dto.UserMetricPagingResponse;
 import com.example.be_study.service.user.dto.UserPrincipalResponse;
 import com.example.be_study.service.user.enums.OauthResponseCode;
 import com.example.be_study.service.user.enums.UserResponseMessage;
 import com.example.be_study.service.user.repository.AdminOnly;
 import com.example.be_study.service.user.repository.UserPrincipal;
+import com.example.be_study.service.user.service.UserMetricService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/user")
-public class UserPrincipalTestController {
+public class UserMissionTestController {
+
+    private final UserMetricService userMetricService;
+
+    public UserMissionTestController(UserMetricService userMetricService) {
+        this.userMetricService = userMetricService;
+    }
 
     @AdminOnly
     @GetMapping("/info")
@@ -26,5 +36,11 @@ public class UserPrincipalTestController {
                 .build();
         return new DataResponse<>(OauthResponseCode.OAUTH_AUTHORIZE_SUCCESS, response);
     }
+
+    @GetMapping("/paging")
+    public Page<UserMetricPagingResponse> getUserPaging(@PageableDefault(page = 1)Pageable pageable){
+        return userMetricService.paging(pageable);
+    }
+
 
 }
--- src/main/java/com/example/be_study/service/user/service/UserMetricService.java
+++ src/main/java/com/example/be_study/service/user/service/UserMetricService.java
@@ -1,8 +1,16 @@
 package com.example.be_study.service.user.service;
 
+import com.example.be_study.service.user.domain.UserMetric;
+import com.example.be_study.service.user.dto.UserMetricPagingResponse;
 import com.example.be_study.service.user.repository.UserMetricRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Dictionary;
 
 @Service
 public class UserMetricService {
@@ -17,4 +25,22 @@ public class UserMetricService {
 
         userMetricRepository.saveAllUserBulkMode();
     }
+
+    public Page<UserMetricPagingResponse> paging(Pageable pageable) {
+        int page = pageable.getPageNumber() - 1;
+        int pageLimit = 50; // 한 페이지에 보여줄 데이터 수
+
+        Page<UserMetric> userMetricPage = userMetricRepository.findAll(PageRequest.of(page, pageLimit,
+                Sort.by(Sort.Direction.ASC,"userId")));
+
+        Page<UserMetricPagingResponse> userMetricPagingResponsesDto = userMetricPage.map(
+                userMetric -> new UserMetricPagingResponse(
+                        userMetric.getUserId(),
+                        userMetric.getAge(),
+                        userMetric.getDeviceType()));
+
+        return userMetricPagingResponsesDto;
+    }
+
+
 }
