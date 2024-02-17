6a8f7edc869009c7da1b62254ff769c30a761550
OreoChoi <chlwnsgh311@gmail.com>
2024-02-17 21:17:05 +0900

    인덱스 추강

--- src/main/resources/db/migration/V20240202235734__create_user_metric.sql
+++ src/main/resources/db/migration/V20240202235734__create_user_metric.sql
@@ -3,3 +3,6 @@ CREATE TABLE user_metric (
     age SMALLINT NULL COMMENT '유저 나이',
     device_type VARCHAR(20) NULL COMMENT '디바이스 타입 - AOS, IOS, WEB'
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE INDEX IDX_USER_ID
+ON user_metric (user_id);
